ce12796e6faca0a70115a37f058f6bb4ae98849f
bakolaszlo <bakolaszlo999@gmail.com>
2021-08-18 09:15:09 +0300

    Imported form, filters and list

---
+++ src/views/Add.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Here you can add employees</h1>
+    <Form />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Form from "@/components/Form.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Form,
+  },
+};
+</script>
